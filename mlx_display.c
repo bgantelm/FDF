ec7645a6b742a6622e0dc8e99f1ab99698bdc5ff
Floren HENRI <fhenri@e1r9p5.42.fr>
2016-01-20 19:52:28 +0100

    test

--- mlx_display.c
+++ mlx_display.c
@@ -6,7 +6,7 @@
 /*   By: fhenri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/16 14:50:41 by fhenri            #+#    #+#             */
-/*   Updated: 2016/01/20 14:20:16 by fhenri           ###   ########.fr       */
+/*   Updated: 2016/01/20 19:50:24 by fhenri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int mlx_display(t_data val, t_data *test)
 		while (b < val.y)
 		{
 			ft_draw_line(a,b,env, test);
-						if (a + 1 < val.x)
-							ft_draw_collone(a,b,env,test);
+			if (a + 1 < val.x)
+				ft_draw_collone(a,b,env,test);
 			b++;
 		}
 		a++; 
@@ -44,99 +44,80 @@ void ft_draw_line(int a, int b, t_my_mlx env, t_data *test)
 	double  x_prime;
 	double y_prime;
 	double x;
-	double coef;
-	int coef_prime =0;
+	double coef = 0;
+	double coef_prime =0;
 	double y;
-
 
 	y_prime = test->tab_final[a][b + 1].y;
 	x_prime =test->tab_final[a][b + 1].x;
 	x = test->tab_final[a][b].x;
 	y = test->tab_final[a][b].y;
-	coef = 14;
-//	if (test->tab_final[a][b].z != 0)
-//	{
+		if (test->tab_final[a][b+1].z != 0 && test->tab_final[a][b].z != test->tab_final[a][b + 1].z)
+		{
 
-//	coef = (x_prime - x )/ test->tab_final[a][b].z;
-//	ft_putnbr(coef);
-		/*
-	ft_putstr("x_prime = ");
-	ft_putnbr((x_prime));
-	ft_putstr("  y_prime =");
-	ft_putnbr((y_prime));
-	ft_putstr("  x =");
-	ft_putnbr((x));
-	ft_putstr("  y =");
-	ft_putnbr((y));
-	ft_putstr("  z =");
-	ft_putnbr((test->tab_final[a][b].z));
-	*/
-//	ft_putstr("\n"); 
-//	}
-	if (test->tab_final[a][b+1].z != 0 && test->tab_final[a][b].z != test->tab_final[a][b + 1].z)
-	{
-		while (x < x_prime)
-		{
-			if (coef_prime == coef && y > y_prime)
+		coef =  (y - y_prime) / ( x - x_prime);
+			while (x < x_prime)
 			{
-				y--;
-				coef_prime = 0;
+				if (coef_prime == coef &&  y > y_prime)
+				{
+					y--;
+					coef_prime = 0;
+				}
+				if (test->tab_final[a][b].z != 0 || test->tab_final[a][b + 1].z != 0)
+				{
+					mlx_pixel_put(env.mlx,env.win,x,y, 0x0066FFFF);
+				}
+				else
+					mlx_pixel_put(env.mlx,env.win,x,y,0x00996600);
+				x++;
+				coef_prime++;
 			}
-			if (test->tab_final[a][b].z != 0 || test->tab_final[a][b + 1].z != 0)
-			{
-				mlx_pixel_put(env.mlx,env.win,x,y, 0x0066FFFF);
-			}
-			else
-				mlx_pixel_put(env.mlx,env.win,x,y,0x00996600);
-			x++;
-			coef_prime++;
 		}
-	}
-	else
-	{
-		while (x < x_prime)
+		else
 		{
-			if (coef_prime == coef && y < y_prime)
+			coef =  (x - x_prime) / ( y - y_prime);
+			while (x < x_prime)
 			{
-				y++;
-				coef_prime = 0;
+				if (coef_prime == coef  &&  y < y_prime)
+				{
+					y++;
+					coef_prime = 0;
+				}
+				if (test->tab_final[a][b].z != 0 || test->tab_final[a][b + 1].z != 0)
+				{
+					mlx_pixel_put(env.mlx,env.win,x,y, 0x0066FFFF);
+				}
+				else
+					mlx_pixel_put(env.mlx,env.win,x,y,0x00996600);
+				x++;
+				coef_prime++;
 			}
-			if (test->tab_final[a][b].z != 0 || test->tab_final[a][b + 1].z != 0)
-			{
-				mlx_pixel_put(env.mlx,env.win,x,y, 0x0066FFFF);
-			}
-			else
-				mlx_pixel_put(env.mlx,env.win,x,y,0x00996600);
-			x++;
-			coef_prime++;
 		}
-	}
 }
 
 void ft_draw_collone(int a, int b, t_my_mlx env, t_data * test)
 {
-	int x_prime;
-	int y_prime;
-	int coef;
-	int coef_prime = 0;
-	int x;
-	int y;
+	double x_prime;
+	double y_prime;
+	double coef = 0;
+	double coef_prime;
+	double x;
+	double y;
 
 
 	x_prime =test->tab_final[a + 1][b].x;
 	y_prime =test->tab_final[a + 1][b].y;
 	x = test->tab_final[a][b].x;
 	y = test->tab_final[a][b].y;
-//	coef = (y_prime - y )/ test->tab_final[a][b].z;
-//	if (coef < 0)
-		coef = 16;
+
+
+	if ( test->tab_final[a + 1][b].z != 0)
+		coef =  (y - y_prime) / (x - x_prime);
 	if (test->tab_final[a + 1][b].z != 0 && test->tab_final[a][b].z != test->tab_final[a + 1][b].z)
 	{
-
-
 		while (y < y_prime)
 		{
-			if (coef_prime == coef && x > x_prime)
+			if ( coef_prime == coef && x > x_prime)
 			{
 				x--;
 				coef_prime = 0;
@@ -155,7 +136,7 @@ void ft_draw_collone(int a, int b, t_my_mlx env, t_data * test)
 	{
 		while (y < y_prime)
 		{
-			if (coef_prime == coef && x < x_prime)
+			if ( coef_prime == coef  && x < x_prime)
 			{
 				x++;
 				coef_prime = 0;
